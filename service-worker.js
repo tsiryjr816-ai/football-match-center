9c266332f7812eb278c7cfa1a1269b94c382cabe
tsiryjr816-ai <tsiryjr816@gmail.com>
2026-08-26 13:03:29 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,9 @@
-self.addEventListener("fetch", () => {});
+const CACHE_NAME = "football-match-center-v2";
+
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+});
